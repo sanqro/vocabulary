8b5f195a298492816f2f7729ff375a446009dbfb
sandro <contact@sanqro.xyz>
2023-02-04 00:31:53 +0100

    lint the files

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,9 +7,7 @@ import "./css/index.css";
 import App from "./pages/App";
 import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
     <App />
@@ -20,4 +18,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
--- client/src/reportWebVitals.ts
+++ client/src/reportWebVitals.ts
@@ -16,4 +16,3 @@ const reportWebVitals = (onPerfEntry?: ReportHandler) => {
 };
 
 export default reportWebVitals;
-
