22790ae02bcb266b121d0eacfbea6fbda470501e
sandro <contact@sanqro.xyz>
2023-02-04 17:59:29 +0100

    change root endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,7 +4,11 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello Typescript from Deta!"));
+app.get("/", (req, res) =>
+  res.status(200).json({
+    msg: "This is the API of the following repository on GithHub: https://github.com/sanqro/vocabulary"
+  })
+);
 
 // Listen on port 3000 if running locally
 if (!process.env.DETA_RUNTIME) {
