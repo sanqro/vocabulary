defa71c08080279ec5c266eb927f44359568a5e3
sanqro <contact@sanqro.xyz>
2023-03-03 10:59:04 +0100

    add interface for vocabulary set

--- server/src/interfaces/interfaces.ts
+++ server/src/interfaces/interfaces.ts
@@ -2,3 +2,11 @@ export interface IAuthForm {
     username: string;
     password: string;
 }
+
+export interface IVocabularySet {
+    key?: string;
+    title: string;
+    creator: string;
+    terms: string[];
+    definitions: string[];
+}
