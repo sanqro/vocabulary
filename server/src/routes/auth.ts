8112e108dcfe3fccd7f98f9028caba79ee6cc8b6
sanqro <contact@sanqro.xyz>
2023-03-03 10:19:29 +0100

    add login endpoint

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -3,6 +3,7 @@ import * as dotenv from "dotenv";
 import path from "path";
 import { Deta } from "deta";
 import argon2 from "argon2";
+import jwt from "jsonwebtoken";
 import { IAuthForm }from "../interfaces/interfaces";
 dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
@@ -10,6 +11,8 @@ dotenv.config({ path: path.resolve(__dirname, "../.env") });
 const projectKey: string = process.env.DETA_PROJECT_KEY;
 const deta = Deta(projectKey);
 const auth = deta.Base("users");
+
+const jwtSecret: string = process.env.JWT_SECRET
 
 const router = express.Router();
 
@@ -37,5 +40,34 @@ router.post("/register", async (req, res) => {
     }
   });
 
+router.post("/login", async (req, res) => {
+    try {
+        const authFormData: IAuthForm = req.body as IAuthForm;
+
+        const user = await auth.get(authFormData.username);
+        const password = user.password as string;
+    
+
+        if (await authFormData.username === null) {
+          throw new Error("This user does not exist. Please register before trying to log in");
+        }
+
+        if (await argon2.verify(password, authFormData.password)) {
+          const token = jwt.sign(
+            { username: user.key },
+            jwtSecret,
+            { expiresIn: "21600s" } 
+          );
+          res.status(200).json({ token, success: true });
+        } else {
+          res.status(401).json({
+            error: "Wrong credentials! Please try again.",
+            success: false
+          });
+        }
+    } catch (err) {
+      res.status(503).json({ error: err.message });
+    }
+  });
 
 export default router;
