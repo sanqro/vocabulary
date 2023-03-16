d082348104cd84cc9ab8a45d7306917470a5f679
sanqro <contact@sanqro.xyz>
2023-03-16 15:24:19 +0100

    build project manually

---
+++ server/dist/routes/sets.js
@@ -0,0 +1,229 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+exports.__esModule = true;
+var express_1 = __importDefault(require("express"));
+var dotenv = __importStar(require("dotenv"));
+var path_1 = __importDefault(require("path"));
+var deta_1 = require("deta");
+var checkUser_1 = __importDefault(require("../middleware/checkUser"));
+dotenv.config({ path: path_1["default"].resolve(__dirname, "../.env") });
+var projectKey = process.env.DETA_PROJECT_KEY;
+var deta = (0, deta_1.Deta)(projectKey);
+var vocabularySet = deta.Base("vocabulary-sets");
+var router = express_1["default"].Router();
+router.post("/create", function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    var vocabularySetData, vocabularySetDataJson, newVocabularySet, err_1;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                _a.trys.push([0, 3, , 4]);
+                vocabularySetData = req.body;
+                return [4, vocabularySet.get(vocabularySetData.title + vocabularySetData.creator)];
+            case 1:
+                if (_a.sent()) {
+                    throw new Error("This vocabulary set exists already. Please try to edit this!");
+                }
+                vocabularySetDataJson = {
+                    key: vocabularySetData.title + vocabularySetData.creator,
+                    title: vocabularySetData.title,
+                    creator: vocabularySetData.creator,
+                    terms: vocabularySetData.terms,
+                    definitions: vocabularySetData.definitions
+                };
+                return [4, vocabularySet.insert(vocabularySetDataJson)];
+            case 2:
+                newVocabularySet = _a.sent();
+                res.status(201).json({
+                    title: vocabularySetDataJson.title,
+                    creator: vocabularySetDataJson.creator,
+                    terms: vocabularySetDataJson.terms,
+                    definitions: vocabularySetDataJson.definitions,
+                    success: true
+                });
+                return [3, 4];
+            case 3:
+                err_1 = _a.sent();
+                res.status(503).json({ error: err_1.message });
+                return [3, 4];
+            case 4: return [2];
+        }
+    });
+}); });
+router.get("/getSet/:id", function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    var fetchedVocabularySet, err_2;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                _a.trys.push([0, 2, , 3]);
+                return [4, vocabularySet.get(req.params.id)];
+            case 1:
+                fetchedVocabularySet = _a.sent();
+                if (fetchedVocabularySet == null) {
+                    res.status(409).json({
+                        error: "This vocabulary set does not exist."
+                    });
+                    return [2, false];
+                }
+                else {
+                    res.status(201).json({ fetchedVocabularySet: fetchedVocabularySet });
+                }
+                return [3, 3];
+            case 2:
+                err_2 = _a.sent();
+                res.status(503).json({ error: err_2.message });
+                return [3, 3];
+            case 3: return [2];
+        }
+    });
+}); });
+router.get("/getAll", function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    var fetchedVocabularySets, err_3;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                _a.trys.push([0, 2, , 3]);
+                return [4, vocabularySet.fetch()];
+            case 1:
+                fetchedVocabularySets = _a.sent();
+                res.status(201).json({ fetchedVocabularySets: fetchedVocabularySets });
+                return [3, 3];
+            case 2:
+                err_3 = _a.sent();
+                res.status(503).json({ error: err_3.message });
+                return [3, 3];
+            case 3: return [2];
+        }
+    });
+}); });
+router["delete"]("/delete/:id", checkUser_1["default"], function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    var fetchedVocabularySet, err_4;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                _a.trys.push([0, 5, , 6]);
+                return [4, vocabularySet.get(req.params.id)];
+            case 1:
+                fetchedVocabularySet = _a.sent();
+                if (!(fetchedVocabularySet != null)) return [3, 3];
+                return [4, vocabularySet["delete"](req.params.id)];
+            case 2:
+                _a.sent();
+                res.status(200).json({ message: "Deleted vocabulary set", id: req.params.id, sucess: true });
+                return [3, 4];
+            case 3:
+                res.status(409).json({
+                    error: "This vocabulary set does not exist."
+                });
+                return [2, false];
+            case 4: return [3, 6];
+            case 5:
+                err_4 = _a.sent();
+                res.status(503).json({ error: err_4.message });
+                return [3, 6];
+            case 6: return [2];
+        }
+    });
+}); });
+router.post("/update", checkUser_1["default"], function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    var vocabularySetData, vocabularySetDataJson, err_5;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                _a.trys.push([0, 4, , 5]);
+                vocabularySetData = req.body;
+                return [4, vocabularySet.get(req.body.oldKey)];
+            case 1:
+                if (!(_a.sent())) {
+                    throw new Error("This vocabulary set does not exist.");
+                }
+                vocabularySetDataJson = {
+                    key: vocabularySetData.title + vocabularySetData.creator,
+                    title: vocabularySetData.title,
+                    creator: vocabularySetData.creator,
+                    terms: vocabularySetData.terms,
+                    definitions: vocabularySetData.definitions
+                };
+                return [4, vocabularySet["delete"](req.body.oldKey)];
+            case 2:
+                _a.sent();
+                return [4, vocabularySet.insert(vocabularySetDataJson)];
+            case 3:
+                _a.sent();
+                res.status(201).json({
+                    title: vocabularySetDataJson.title,
+                    creator: vocabularySetDataJson.creator,
+                    terms: vocabularySetDataJson.terms,
+                    definitions: vocabularySetDataJson.definitions,
+                    success: true
+                });
+                return [3, 5];
+            case 4:
+                err_5 = _a.sent();
+                res.status(503).json({ error: err_5.message });
+                return [3, 5];
+            case 5: return [2];
+        }
+    });
+}); });
+exports["default"] = router;
