5d83bfbca0a76683a74790f2c68fc94bc309c78b
sanqro <contact@sanqro.xyz>
2023-03-18 19:54:11 +0100

    forgot to commit page

--- client/src/pages/LearnVocabulary.tsx
+++ client/src/pages/LearnVocabulary.tsx
@@ -1,22 +1,14 @@
 import React from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-
-function startLearn() {
-  alert("You are learning now");
-}
+import LearnView from "../components/LearnView";
 
 function LearnVocabulary() {
   return (
     <div className="flex flex-col h-screen">
       <Header></Header>
-      <main id="body">
-        <button
-          onClick={startLearn}
-          className=" py-2 px-4 text-white rounded mx-10% bg-blue-500 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-        >
-          Start
-        </button>
+      <main>
+        <LearnView></LearnView>
       </main>
       <Footer></Footer>
     </div>
