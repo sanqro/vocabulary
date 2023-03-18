a390e4d7f9cf7fee817405157a8204b4b85acf03
sanqro <contact@sanqro.xyz>
2023-03-18 22:03:44 +0100

    remove duplicate interfaces

--- client/src/interfaces/props.ts
+++ client/src/interfaces/props.ts
@@ -1,15 +1,16 @@
 export interface ILinkButton {
   destination: string;
   children: string;
+  className?: string;
 }
 
 export interface IVocabularySet {
-  oldKey?: string;
-  key?: string;
-  title?: string;
-  creator?: string;
-  terms?: string[];
-  definitions?: string[];
+  oldKey: string;
+  key: string;
+  title: string;
+  creator: string;
+  terms: string[];
+  definitions: string[];
 }
 
 export interface IFetchedVocabularySets {
@@ -22,48 +23,6 @@ export interface ILearnModeData {
   definitions: string[];
   title: string;
   creator: string;
-}
-
-export interface IOnClickButton {
-  onClick: () => void;
-  label: string;
-  className?: string;
-}
-
-export interface IVocabularyInput {
-  definition: string[];
-  term: string[];
-}
-
-export interface IInputWord {
-  type: string;
-  placeholder?: string;
-  value: string;
-  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-}
-
-export interface IEditView {
-  children: string;
-}
-
-export interface ILinkButton {
-  destination: string;
-  children: string;
-  className?: string;
-}
-
-export interface IVocabularySet {
-  oldKey?: string;
-  key?: string;
-  title?: string;
-  creator?: string;
-  terms?: string[];
-  definitions?: string[];
-}
-
-export interface IFetchedVocabularySets {
-  items: IVocabularySet[];
-  count: number;
 }
 
 export interface IOnClickButton {
