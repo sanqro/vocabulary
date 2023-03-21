18dc1639d35b7b77243ba99ead1b02eb32110cad
sanqro <contact@sanqro.xyz>
2023-03-21 20:04:05 +0100

    fix not found page

--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import LinkButton from "../components/LinkButton";
 
 function notFound() {
   return (
     <div className="flex flex-col h-screen">
-      <Header></Header>
-      <main id="body" className="flex-grow m-3 mb-4">
-        Page was not found
+      <Header />
+      <main className="flex flex-col items-center justify-center flex-grow">
+        <h1 className="text-3xl font-bold mb-4">404 - Page Not Found</h1>
+        <LinkButton className="py-2 px-4 text-white rounded mx-10% bg-blue-500" destination="/">
+          Go home
+        </LinkButton>
       </main>
-      <Footer></Footer>
+      <Footer />
     </div>
   );
 }
