1973b49f934ccad2dfbf68c292ec364e3c577796
sanqro <contact@sanqro.xyz>
2023-03-21 19:28:47 +0100

    fix props

--- client/src/components/LinkButton.tsx
+++ client/src/components/LinkButton.tsx
@@ -5,7 +5,7 @@ import { ILinkButton } from "../interfaces/props";
 function linkButton(props: ILinkButton) {
   return (
     <Link to={props.destination as To}>
-      <button className="py-2 px-4 border border-black rounded mx-10%">{props.children}</button>
+      <button className={props.className}>{props.children}</button>
     </Link>
   );
 }
