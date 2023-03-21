cfefba9500c10f79b809dccdbbfe599740beb71b
sanqro <contact@sanqro.xyz>
2023-03-21 22:49:49 +0100

    delete unused files

--- client/src/components/ViewVocab.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { IEditView } from "../interfaces/props";
-
-function viewVocab(props: IEditView) {
-  return <div>{props.children}</div>;
-}
-
-export default viewVocab;
